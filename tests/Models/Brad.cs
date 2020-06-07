e03b94b29a905effba8f639137f9b7b815d24a80
Bradley Krishna <21622006@dut4life.ac.za>
2020-06-07 22:45:03 +0200

    hmmm

---
+++ tests/Models/Brad.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tests.Models
+{
+    public class Brad
+    {
+        public string brad { get; set; }
+    }
+}
